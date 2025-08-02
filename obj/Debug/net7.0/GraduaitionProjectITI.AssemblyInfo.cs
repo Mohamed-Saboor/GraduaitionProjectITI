3cac5a3c0a2af975cdbf8e8708ff1f5337d67286
NDS <NDS@DESKTOP-O2CFSJH>
2025-08-02 14:45:39 +0300

    Clonesaboor

--- obj/Debug/net7.0/GraduaitionProjectITI.AssemblyInfo.cs
+++ obj/Debug/net7.0/GraduaitionProjectITI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraduaitionProjectITI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b86c6ff4c665788828e119b17e0aa4848fe75d4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraduaitionProjectITI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraduaitionProjectITI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
